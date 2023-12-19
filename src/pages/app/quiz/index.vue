f686f6ea24f97aa8696b710678c39ba931508e26
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-19 18:38:08 +0800

    change icon to photo

--- src/pages/app/quiz/index.vue
+++ src/pages/app/quiz/index.vue
@@ -5,7 +5,7 @@
   <div class="p-4">
     <n-h1>Select quiz</n-h1>
     <router-link class="flex flex-col items-center gap-6 rounded-xl bg-gray-300 px-2 py-8 dark:bg-gray-600" to="/quiz/kinematics">
-      <i-angle class="h-24 w-24 shrink-0 rounded-full bg-red-500 p-4" />
+      <img :src="`/images/kinematics-icon.png`" class="h-24 w-24">
       <div class="flex flex-col justify-center gap-2">
         <h1 class="text-center  text-xl font-semibold leading-none">
           Kinematics
@@ -13,7 +13,7 @@
       </div>
     </router-link>
     <router-link class="mt-3 flex flex-col items-center gap-6 rounded-xl bg-gray-300 px-2 py-8 dark:bg-gray-600" to="/quiz/newtons-law">
-      <i-arrows-exchange-2 class="h-24 w-24 shrink-0 rounded-full bg-blue-500 p-4" />
+      <img :src="`/images/newton-icon.png`" class="h-24 w-24" />
       <div class="flex flex-col justify-center gap-2">
         <h1 class="text-center  text-xl font-semibold leading-none">
           Newton's Laws of Motion
