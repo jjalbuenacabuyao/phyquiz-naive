de11c63148adf68e8536a5ee50b72ab847f1d59c
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-19 18:37:48 +0800

    style the radio button

--- src/pages/app/quiz/[id].vue
+++ src/pages/app/quiz/[id].vue
@@ -1,83 +1,102 @@
 <script setup lang="tsx">
 //@ts-nocheck
-import quizzes from '@/content/quizzes'
+import quizzes from "@/content/quizzes";
 
-const route = useRoute()
-const router = useRouter()
-const quizId = route.params.id
-const questions = quizzes[quizId].questions
+const route = useRoute();
+const router = useRouter();
+const quizId = route.params.id;
+const questions = quizzes[quizId].questions;
 
-const randomizedQuestions = questions.map((q, i) => ({ ...q, id: i })).sort(() => Math.random() - 0.5)
-const quizState = ref<Array<{
-  questionNumber: number
-  answer: string
-}>>([])
-const selectedAnswer = ref()
-const currentQuestion = ref(randomizedQuestions[0])
-const message = useMessage()
+const randomizedQuestions = questions
+  .map((q, i) => ({ ...q, id: i }))
+  .sort(() => Math.random() - 0.5);
+const quizState = ref<
+  Array<{
+    questionNumber: number;
+    answer: string;
+  }>
+>([]);
+const selectedAnswer = ref();
+const currentQuestion = ref(randomizedQuestions[0]);
+const message = useMessage();
 
 async function next() {
   quizState.value.push({
     questionNumber: currentQuestion.value.id,
     answer: selectedAnswer.value,
-  })
-  selectedAnswer.value = undefined
+  });
+  selectedAnswer.value = undefined;
   if (quizState.value.length === randomizedQuestions.length) {
     // Use crypto to generate UUID
-    const id = crypto.randomUUID()
+    const id = crypto.randomUUID();
     const status = {
       id,
       state: quizState.value,
       quizId,
       createdAt: new Date().toISOString(),
       score: quizState.value.reduce((acc, curr) => {
-        const question = questions[curr.questionNumber]
-        const correctAnswer = question.choices.find(c => c.correct)
-        if (curr.answer === correctAnswer.text)
-          return acc + 1
-        return acc
+        const question = questions[curr.questionNumber];
+        const correctAnswer = question.choices.find((c) => c.correct);
+        if (curr.answer === correctAnswer.text) return acc + 1;
+        return acc;
       }, 0),
-    }
+    };
 
-    const history = JSON.parse(localStorage.getItem('history') || '[]')
-    history.push(status)
-    localStorage.setItem('history', JSON.stringify(history))
+    const history = JSON.parse(localStorage.getItem("history") || "[]");
+    history.push(status);
+    localStorage.setItem("history", JSON.stringify(history));
 
-    await nextTick()
-    message.success('Quiz completed!')
-    router.push(`/history/${id}`)
-  }
-  else {
-    currentQuestion.value = randomizedQuestions[quizState.value.length]
+    await nextTick();
+    message.success("Quiz completed!");
+    router.push(`/history/${id}`);
+  } else {
+    currentQuestion.value = randomizedQuestions[quizState.value.length];
   }
 }
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-4 bg-slate-300 dark:bg-slate-700">
     <div class="text-gray-500 dark:text-gray-400">
       Question {{ quizState.length + 1 }}/{{ randomizedQuestions.length }}
     </div>
     <p class="mt-2 text-xl">
       {{ currentQuestion?.question }}
     </p>
-    <n-radio-group v-if="currentQuestion" v-model:value="selectedAnswer" class="mt-4" name="choices" size="large">
-      <div class="flex flex-col gap-3">
+    <n-radio-group
+      v-if="currentQuestion"
+      v-model:value="selectedAnswer"
+      class="mt-4"
+      name="choices"
+      size="large"
+      style="display: block !important"
+    >
+      <div class="flex flex-col gap-2">
         <n-radio
           v-for="choice in currentQuestion.choices"
           :key="choice.text"
           :label="choice.text"
           :value="choice.text"
+          class="p-3 rounded-full bg-white dark:bg-slate-800"
         />
       </div>
     </n-radio-group>
     <div class="mt-4 flex justify-end">
-      <n-button :disabled="!selectedAnswer" icon-placement="right" type="primary" @click="next">
+      <n-button
+        :disabled="!selectedAnswer"
+        icon-placement="right"
+        type="primary"
+        @click="next"
+      >
         <template #icon>
           <i-arrow-narrow-right />
         </template>
 
-        {{ quizState.length + 1 === randomizedQuestions.length ? 'Finish' : 'Next' }}
+        {{
+          quizState.length + 1 === randomizedQuestions.length
+            ? "Finish"
+            : "Next"
+        }}
       </n-button>
     </div>
   </div>
