32c5906e570594e6d9166b28c544073da6422af7
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-19 18:39:02 +0800

    use photo instead of icon

--- src/pages/app/topic/index.vue
+++ src/pages/app/topic/index.vue
@@ -10,8 +10,8 @@ definePage({
 <template>
   <div class="p-4">
     <router-link v-for="item in topics" class="mb-3 flex flex-col items-center gap-6 rounded-xl bg-gray-300 p-6 dark:bg-gray-600" :to="`/topic/${item.id}`">
-      <i-angle v-if="item.id === 'kinematics'" class="h-24 w-24 shrink-0 rounded-full bg-red-500 p-4" />
-      <i-arrows-exchange-2 v-else class="h-24 w-24 shrink-0 rounded-full bg-blue-500 p-4" />
+      <img v-if="item.id === 'kinematics'" :src="`/images/kinematics-icon.png`" class="h-24 w-24">
+      <img v-else :src="`/images/newton-icon.png`" class="h-28 w-28">
 
       <div class="flex flex-col justify-center gap-2">
         <h1 class="text-center  text-xl font-semibold leading-none">
