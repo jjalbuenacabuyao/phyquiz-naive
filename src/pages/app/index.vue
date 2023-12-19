2c62d5cf2772337d3ab91bd314cb895d9503bcb6
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2023-12-19 18:35:04 +0800

    rearrange elements

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -10,21 +10,7 @@ definePage({
 
 <template>
   <div class="p-4">
-    <router-link class="card" to="/quiz">
-      <!-- Pinagbaliktad -->
-      <div class="flex flex-col justify-center gap-2">
-        <h1 class="text-2xl font-semibold leading-none">
-          Play the Quiz!
-        </h1>
-        <p>
-          Test your knowledge about Kinematics and Newton's Laws of Motion
-        </p>
-      </div>
-      <div class="h-28 w-28 shrink-0 rounded-lg bg-gradient-to-br from-sky-400 via-red-200 to-violet-800 pl-1 pt-2">
-        <i-player-play-filled class="h-24 w-24 text-white" />
-      </div>
-    </router-link>
-    <hr>
+
     <div class="card flex-col">
       <h1 class="flex items-center gap-3 text-lg font-semibold">
         <i-book class="text-xl" />
@@ -32,8 +18,9 @@ definePage({
       </h1>
       <router-link v-for="item in topics" :to="`/topic/${item.id}`">
         <div class="flex rounded-xl bg-gray-300 p-4 dark:bg-gray-600">
-          <i-angle v-if="item.id === 'kinematics'" class="h-24 w-24 shrink-0 rounded-full bg-red-500 p-4" />
-          <i-arrows-exchange-2 v-else class="h-24 w-24 shrink-0 rounded-full bg-blue-500 p-4" />
+          <img v-if="item.id === 'kinematics'" :src="`/images/kinematics-icon.png`" class="h-20 w-20">
+
+          <img v-else :src="`/images/newton-icon.png`" class="h-24 w-24">
 
           <div class="ml-6 flex flex-col justify-center gap-2">
             <h1 class="text-xl font-semibold leading-none">
@@ -46,6 +33,7 @@ definePage({
         </div>
       </router-link>
     </div>
+
     <div class="card mt-3 flex-col">
       <h1 class="flex items-center gap-3 text-lg font-semibold">
         <i-users class="text-xl" />
@@ -69,6 +57,9 @@ definePage({
         </div>
       </router-link>
     </div>
+
+    <hr>
+
     <router-link class="card mt-3" to="/formulas">
       <i-math-symbols class="h-28 w-28 shrink-0 rounded-lg bg-white p-2 text-green-600" />
       <div class="flex flex-col justify-center gap-2">
@@ -80,6 +71,7 @@ definePage({
         </p>
       </div>
     </router-link>
+
     <router-link class="card mt-3" to="/simulations">
       <i-bulb class="h-28 w-28 shrink-0 rounded-lg bg-white p-2 text-amber-500" />
       <div class="flex flex-col justify-center gap-2">
@@ -91,6 +83,21 @@ definePage({
         </p>
       </div>
     </router-link>
+
+    <router-link class="card mt-2" to="/quiz">
+      <div class="h-28 w-28 shrink-0 rounded-lg bg-gradient-to-br from-sky-400 via-red-200 to-violet-800 pl-1 pt-2">
+        <i-player-play-filled class="h-24 w-24 text-white" />
+      </div>
+      <div class="flex flex-col justify-center gap-2">
+        <h1 class="text-2xl font-semibold leading-none">
+          Play the Quiz!
+        </h1>
+        <p>
+          Test your knowledge about Kinematics and Newton's Laws of Motion
+        </p>
+      </div>
+    </router-link>
+
   </div>
 </template>
 
